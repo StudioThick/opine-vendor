084ea36db0cb8699fc4cd25294ef8ceb12e5fc1d
Giovanni Ferron <gvnnrulez@gmail.com>
2016-09-01 20:08:50 +1000

    Add minification if not in debug mode.
    Most vendors produce a minified version, but there are some libraries that require minification. Adding only if not debug.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var opine = require('gulp-opine');
-
+var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
 var module = opine.module('vendor');
@@ -9,13 +9,20 @@ var module = opine.module('vendor');
 var includes = module.getConfig('includes', []);
 var target = module.getConfig('target', 'vendor.js');
 var dest = opine.getDest('scripts');
+var debug = module.getConfig('debug', true);
 
 module.addBuild();
 module.addWatch(includes);
 
 module.task(function() {
-    return gulp.src(includes)
-        .pipe(concat(target))
+    var b = gulp.src(includes)
+        .pipe(concat(target));
+
+    if(!debug) {
+        b = b.pipe(uglify());
+    }
+
+    return b
         .pipe(gulp.dest(dest))
         .pipe(module.size());
 });
